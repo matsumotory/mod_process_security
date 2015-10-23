e4ee820c3137047fa5b43be91df817dab1336762
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-10-23 15:03:40 +0900

    Refactor log message

--- mod_process_security.c
+++ mod_process_security.c
@@ -506,7 +506,7 @@ static int process_security_handler(request_rec *r)
   ugid = ap_run_get_suexec_identity(r);
   if (dconf->check_suexec_ids == ON && ugid != NULL && (ugid->uid != r->finfo.user || ugid->gid != r->finfo.group)) {
     ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL,
-        "%s ERROR %s: PSCheckSuexecids faild: opened r->filename=%s uid=%d gid=%d but suexec config uid=%d gid=%d",
+        "%s ERROR %s: PSCheckSuexecids return 403: opened r->filename=%s uid=%d gid=%d but suexec config uid=%d gid=%d",
         MODULE_NAME, __func__, r->filename, r->finfo.user, r->finfo.group, ugid->uid, ugid->gid);
     return HTTP_FORBIDDEN;                                                       
   }                                                                              
